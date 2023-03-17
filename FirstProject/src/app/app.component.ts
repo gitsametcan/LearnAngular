d8fe6eb561bfefbe18db5fc49eadf1b833e011b7
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2023-03-17 23:17:35 +0300

    Progress on learning

--- FirstProject/src/app/app.component.ts
+++ FirstProject/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstProject';
+  // burada default public tanımlanır ama private verilerek 
+  // oge html tarafından saklanabilir
+  // asagıdaki gibi get kullanılabilir
+
+  title = 'First Project';
+  private deneme = "yapacaz";
+
+  item = {
+    aciklama: "bisiler",
+    sayi: 5
+  }
+
+  getTitle() {
+    return this.deneme;
+  }
 }
