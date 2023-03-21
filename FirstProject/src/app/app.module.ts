74be2fbf7b905bdddccb83d0ecc4525665752776
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2023-03-21 22:37:39 +0300

    Fixing folders

--- FirstProject/src/app/app.module.ts
+++ FirstProject/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstTryComponent } from './firsttry.component';
 import { SecondtryComponent } from './secondtry/secondtry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstTryComponent,
     SecondtryComponent
   ],
   imports: [
